48f2c394274ad31ea61c7a8fa5eea28cb9a9be3b
Sergio Orbe <reyiyo@gmail.com>
2017-11-04 12:27:29 -0300

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # Validate environment variables
 : "${UPSTREAM_HOST:?Set UPSTREAM_HOST using --env}"
-: "${UPSTREAM_PORT?Set UPSTREAM_PORT using --env}"
+: "${UPSTREAM_PORT:?Set UPSTREAM_PORT using --env}"
 PROTOCOL=${PROTOCOL:=HTTP}
 
 # Template an nginx.conf
